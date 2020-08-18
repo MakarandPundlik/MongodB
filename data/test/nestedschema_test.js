f3bd4df9c671565e728bec2a702a0048c08c182d
MAK23AUG <63389397+MAK23AUG@users.noreply.github.com>
2020-08-18 20:13:47 +0530

    Update nestedschema_test.js

--- data/test/nestedschema_test.js
+++ data/test/nestedschema_test.js
@@ -14,5 +14,12 @@ describe('it consists of nested schema',(done)=>{
             assert(res.books.length === 1);
             done();
         })
-    })
+    });
+
+    it('adds a book to existing author',()=>{
+        AuthorChar.findOne({name:'APJ Abdul Kalam'}).then((res)=>{
+            res.books.push({title:'Vision 20-20',pages:500});
+            done();
+        });
+    });
 });
